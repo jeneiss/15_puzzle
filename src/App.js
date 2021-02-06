6cbcb94928453de54c520142c85fe9d306deabb2
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-02-06 12:25:16 +0000

    Add basic tile display and state to Board

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const initialConfiguration = [1, 2, 0, 4, 5, 6, 3, 8, 9, 10, 7, 12, 13, 14, 11, 15]
 
   const onSolveCallback = () => {
-    alert('Well done you solved it')
+    alert('Well done, you solved it!')
   }
 
   return (
--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Board.css';
 
 function Board({ initialConfiguration, onSolveCallback }) {
+  const [tileConfig, setTileConfig] = useState(initialConfiguration)
+
   return (
-    null
+    <div className='board'>
+      {tileConfig.map((tile, index) => {
+        return (
+          <div
+            className={tile === 0 ? 'empty' : 'tile'}
+            key={index}
+          >
+            {tile === 0 ? null : tile}
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
