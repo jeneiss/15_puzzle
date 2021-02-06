dd18fc6fd8453069ff258a7f08e3b495e49b0429
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-02-06 14:06:20 +0000

    Add movement to tiles

--- src/Board.js
+++ src/Board.js
@@ -4,13 +4,37 @@ import './Board.css';
 function Board({ initialConfiguration, onSolveCallback }) {
   const [tileConfig, setTileConfig] = useState(initialConfiguration)
 
+  const handleClick = (e) => {
+    const num = e.target.innerText === '' ? 0 : parseInt(e.target.innerText)
+    const numIndex = tileConfig.findIndex(el => el === num)
+
+    //Have index, need to check if indexes around in tileConfig contain 0.
+    const newTileConfig = [...tileConfig]
+    if (tileConfig[numIndex + 1] === 0) {
+      newTileConfig[numIndex + 1] = num
+      newTileConfig[numIndex] = 0
+    } else if (tileConfig[numIndex - 1] === 0) {
+      newTileConfig[numIndex - 1] = num
+      newTileConfig[numIndex] = 0
+    } else if (tileConfig[numIndex + 4] === 0) {
+      newTileConfig[numIndex + 4] = num
+      newTileConfig[numIndex] = 0
+    } else if (tileConfig[numIndex - 4] === 0) {
+      newTileConfig[numIndex - 4] = num
+      newTileConfig[numIndex] = 0
+    }
+
+    setTileConfig(newTileConfig)
+  }
+
   return (
     <div className='board'>
-      {tileConfig.map((tile, index) => {
+      {tileConfig.map((tile) => {
         return (
           <div
             className={tile === 0 ? 'empty' : 'tile'}
-            key={index}
+            key={tile}
+            onClick={tile === 0 ? null : handleClick}
           >
             {tile === 0 ? null : tile}
           </div>
